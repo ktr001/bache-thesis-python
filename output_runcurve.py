a4ae8aee0cebf189084b53cbcb46e6d2ee2d7453
Iori KOTERA <kotera8300iori@gmail.com>
2024-10-03 11:03:02 +0900

    created BPF_omited_firstzeros. (the data from the start to 087:29:20.169 is omitted)

--- output_runcurve.py
+++ output_runcurve.py
@@ -1,6 +1,7 @@
 # %% 
 from os import name
 import plotly.graph_objects as go
+from plotly import offline
 import numpy as np
 import polars as pl
 
@@ -14,7 +15,7 @@ pprint.pprint(CURRENT_DIR)
 #%%
 
 PATH_TO_DATA_DIR : str = "/home/iori/daxue/bache_thesis/20240629_down_Futamata_to_Shinjohara/"
-PATH_TO_SOURCE_FILE :str = PATH_TO_DATA_DIR + "gps_distance_omitted_firstzeros_1st1000.csv"
+PATH_TO_SOURCE_FILE :str = PATH_TO_DATA_DIR + "gps_distance_omitted_firstzeros.csv"
 
 df = pl.read_csv(PATH_TO_SOURCE_FILE).select(["localtime_datetime","speed","distance/m"])
 #%%
@@ -22,3 +23,22 @@ local_datetime = np.array(df["localtime_datetime"])
 speed = np.array(df["speed"])
 distance = np.array(df["distance/m"])
 #%%
+fig = go.Figure(
+    data = [
+        go.Scatter(x=local_datetime,y=speed*3.6,name='speed'),
+        go.Scatter(x=local_datetime,y=distance/1000,name='distance'),
+    ]
+).update_layout(
+    title='run curve(km/h,km)',
+    legend=dict(
+        xanchor='left',
+        yanchor='bottom',
+        x=0.02,
+        y=0.9,
+        orientation='h',
+        )
+)
+
+fig.show()
+offline.plot(fig,filename='run_curve_0629down')
+# %%
