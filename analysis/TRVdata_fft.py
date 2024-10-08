571a1cb7b0a2e49408287aaa14840ad150d3b597
Iori KOTERA <kotera8300iori@gmail.com>
2024-10-09 01:42:01 +0900

    reset to 746f6a7c

---
+++ analysis/TRVdata_fft.py
@@ -0,0 +1,213 @@
+#%%
+from os import name
+import datetime
+import plotly.graph_objects as go
+from plotly import offline
+import numpy as np
+import numpy.typing as npt
+from bache_thesis_python import fft
+import polars as pl
+from pathlib import Path
+import pprint
+
+CURRENT_DIR = Path.cwd()
+pprint.pprint(f'__name__ = {__name__}')
+pprint.pprint(f'__name__ == "__main__" = {__name__ == "__main__"}')
+pprint.pprint(CURRENT_DIR)
+
+#%%
+
+PATH_TO_0630_DATA_DIR : str = "/home/iori/daxue/bache_thesis/20230630_軌道変位データ/"
+SOURCE_0630_FILE :str = PATH_TO_0630_DATA_DIR + "TGdata20230630_converted.csv"
+# PATH_TO_0126_DATA_DIR : str = "/home/iori/daxue/bache_thesis/20240126_軌道変位データ/"
+# SOURCE_0126_FILE :str = PATH_TO_0126_DATA_DIR + "TGdata20240126_converted.csv"
+
+# SOURCE_0630_FILE :str= PATH_TO_0630_DATA_DIR + "TGdata_freqtestdata_1st1000.csv"
+#%%
+###### FFT (debug)
+# def graph_fft(raw_data):
+#     num_of_data = len(raw_data)
+#     SAMPLE_FREQ = 1/(0.25) #空間周波数[1/m]
+#     F = np.fft.fft(raw_data)
+#     Amp = np.abs( F / (num_of_data / 2) )
+
+#     freq = np.fft.fftfreq(num_of_data, d= 1/SAMPLE_FREQ)
+#     n_freq_half : int = len(freq) // 2
+#     fft_figdata:list[go.Scatter] = [
+#         go.Scatter(x = freq[0:n_freq_half], y = Amp[0:n_freq_half]),
+#     ]
+#     fft_fig:go.Figure = go.Figure(data = fft_figdata)
+#     fft_fig.show()
+
+# def dft(w,t,G):
+#     def expi(x):
+#         return np.exp(complex(0.,x))
+#     res = 0.+0.j
+#     for i in range(len(t)-1):
+#         tx,ty = t[i],t[i+1]
+#         Gx,Gy = G[i],G[i+1]
+#         dt = ty-tx
+#         exwdt = expi(-w*dt)
+#         d = (complex(1.0,-w*dt) - exwdt )/(w**2*dt)
+#         dc= d.conjugate()                
+#         res += (Gx*d + Gy*dc * exwdt) * expi(-w*tx)
+#     return w, res.real, res.imag
+
+
+##### FFT end
+# %%
+# t = np.array([0.001*(2**i) for i in range(100)])
+# t = np.array([0.1*(i) for i in range(1000)])
+# wave = np.sin(2*np.pi*t) + 0.5*np.sin(2*0.5*np.pi*t)
+# #%%
+# graph_fft(raw_data=wave)
+# # %%
+# result_data:dict[str,npt.NDArray] = fft.calc_amp(wave,10)['result_half']
+# df_fft_result : pl.DataFrame = pl.DataFrame(result_data).with_columns( (pl.col('freq')**(-1)).alias('wave_length') )
+#%%
+# go.Figure(
+#     data = [
+#         go.Scatter(x = df_fft_result['wave_length'], y = df_fft_result['amp'])
+#     ]
+# ).show()
+#%%
+df_source_data = pl.read_csv(SOURCE_0630_FILE) # sourxe data 読み込み
+# graph_fft(df_twist["平面性(生波形)"])
+df_twist_fft_result:pl.DataFrame =pl.DataFrame(
+     fft.calc_amp(np.array(df_source_data['平面性(生波形)']),1/(0.25))['result_half']  
+             ).with_columns( 
+                 (pl.col('freq')**(-1)
+                  ).alias('wave_length')
+                    ) ## twistについてfft =>逆数取って波長求める
+
+df_crosssection_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['水準(生波形)']
+        ),
+        fs=1/(0.25),
+    )['result_half']
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+df_longitudinal_R_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['10m弦高低(右)(狂い量)']
+        ),
+        fs=1/(0.25),
+    )['result_half'],
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+df_longitudinal_L_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['10m弦高低(左)(狂い量)']
+        ),
+        fs=1/(0.25),
+    )['result_half'],
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+df_alignment_R_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['10m弦通り(右)(生波形)']
+        ),
+        fs=1/(0.25),
+    )['result_half'],
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+df_alignment_L_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['10m弦通り(左)(生波形)']
+        ),
+        fs=1/(0.25),
+    )['result_half'],
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+df_gauge_fft_result:pl.DataFrame = pl.DataFrame(
+    fft.calc_amp(
+        np.array(
+            df_source_data['軌間(生波形)']
+        ),
+        fs=1/(0.25),
+    )['result_half']
+).with_columns(
+    (pl.col('freq')**(-1)).alias('wave_length')
+)
+
+#%%
+fig_wavelength_analysis: go.Figure = (
+    go.Figure(
+        data = [
+            go.Scatter(x = df_gauge_fft_result['wave_length'], y = df_gauge_fft_result['amp'],name='Gauge'), 
+            go.Scatter(x = df_alignment_R_fft_result['wave_length'], y = df_alignment_R_fft_result['amp'],name='Alignment(R)'), 
+            go.Scatter(x = df_alignment_L_fft_result['wave_length'], y = df_alignment_L_fft_result['amp'],name='Alignment(L)'),
+            go.Scatter(x = df_longitudinal_R_fft_result['wave_length'], y = df_longitudinal_R_fft_result['amp'],name='Longitudinal(R)'),
+            go.Scatter(x = df_longitudinal_L_fft_result['wave_length'], y = df_longitudinal_L_fft_result['amp'],name='Longitudinal(L)'),
+            go.Scatter(x = df_crosssection_fft_result['wave_length'], y = df_crosssection_fft_result['amp'],name='Cross Level'),
+            go.Scatter(x = df_twist_fft_result['wave_length'], y = df_twist_fft_result['amp'],name='Twist'),
+        ]
+    ).update_layout(
+        xaxis = dict(title = 'wave length(m)'),
+        yaxis = dict(title = 'amplitude'),
+        # title ='wavelength analysis of TRV data 0630 for 1st 1000rows ',
+        title ='wavelength analysis of TRV data 0630',
+        legend=dict(
+            xanchor='left',
+            yanchor='bottom',
+            x=0.02,
+            y=0.9,
+            orientation='h',
+            ),
+        )
+)
+
+# %%
+fig_freq_analysis = (
+    go.Figure(
+        data = [
+            go.Scatter(x = df_gauge_fft_result['freq'], y = df_gauge_fft_result['amp'],name='Gauge'), 
+            go.Scatter(x = df_alignment_R_fft_result['freq'], y = df_alignment_R_fft_result['amp'],name='Alignment(R)'), 
+            go.Scatter(x = df_alignment_L_fft_result['freq'], y = df_alignment_L_fft_result['amp'],name='Alignment(L)'),
+            go.Scatter(x = df_longitudinal_R_fft_result['freq'], y = df_longitudinal_R_fft_result['amp'],name='Longitudinal(R)'),
+            go.Scatter(x = df_longitudinal_L_fft_result['freq'], y = df_longitudinal_L_fft_result['amp'],name='Longitudinal(L)'),
+            go.Scatter(x = df_crosssection_fft_result['freq'], y = df_crosssection_fft_result['amp'],name='Cross Level'),
+            go.Scatter(x = df_twist_fft_result['freq'], y = df_twist_fft_result['amp'],name='Twist'),
+        ]
+    ).update_layout(
+        xaxis = dict(title = 'frequency(1/m)'),
+        yaxis = dict(title = 'amplitude'),
+        # title ='frequency analysis of TRV data 0630 for 1st 1000rows ',
+        title ='frequency analysis of TRV data 0630',
+        legend=dict(
+            xanchor='left',
+            yanchor='bottom',
+            x=0.02,
+            y=0.9,
+            orientation='h',
+            ),
+        )
+)
+
+fig_wavelength_analysis.show()
+fig_freq_analysis.show()
+# %%
+if __name__ == '__main__':
+
+    # offline.plot(figure_or_data = fig_wavelength_analysis, filename = PATH_TO_0630_DATA_DIR+'fig_wavelength_analysis_for1st1000data.html')
+    # offline.plot(figure_or_data = fig_freq_analysis, filename = PATH_TO_0630_DATA_DIR+'fig_freq_analysis_for1st1000data.html' )
+
+    offline.plot(figure_or_data = fig_wavelength_analysis, filename = PATH_TO_0630_DATA_DIR+'fig_wavelength_analysis.html')
+    offline.plot(figure_or_data = fig_freq_analysis, filename = PATH_TO_0630_DATA_DIR+'fig_freq_analysis.html' )
+# %%
